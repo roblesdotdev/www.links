5c332f47e9e276c39fcc75d1975198722690221f
roblesdotdev <httpsnip@gmail.com>
2024-08-12 23:30:24 -0300

    initial theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import { type Config } from 'tailwindcss'
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -12,11 +13,48 @@ export default {
         lg: '65ch',
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      canvas: 'hsl(var(--canvas) / <alpha-value>)',
+      accent: 'hsl(var(--accent) / <alpha-value>)',
+      fg: {
+        DEFAULT: 'hsl(var(--fg-default) / <alpha-value>)',
+        muted: 'hsl(var(--fg-muted) / <alpha-value>)',
+      },
+      border: 'hsl(var(--border) / <alpha-value>)',
+    },
     extend: {
       screens: {
         xs: '475px',
       },
+      keyframes: {
+        'slide-up': {
+          from: {
+            opacity: '0',
+            transform: 'translateY(20px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+        'slide-down': {
+          from: {
+            opacity: '0',
+            transform: 'translateY(-20px)',
+          },
+          to: {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+      animation: {
+        'slide-up': 'slide-up 0.3s ease-out backwards',
+        'slide-down': 'slide-down 0.3s ease-out backwards',
+      },
     },
   },
   plugins: [],
-}
+} satisfies Config
