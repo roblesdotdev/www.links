a9c06792db33b15a99810bc93b43dac0df49aa80
roblesdotdev <httpsnip@gmail.com>
2024-08-16 21:00:15 -0300

    add sitemap & og img

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 
+import sitemap from '@astrojs/sitemap'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://www-links.pages.dev',
   compressHTML: true,
   prefetch: true,
   server: {
@@ -15,5 +18,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     icon(),
+    sitemap(),
   ],
 })
